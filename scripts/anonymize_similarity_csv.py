86b35c1e6e82cdd40bd6de54f8b740905430a03c
Niema Moshiri <niemamoshiri@gmail.com>
2021-07-23 10:42:11 -0700

    Added similarity distributions

--- scripts/anonymize_similarity_csv.py
+++ scripts/anonymize_similarity_csv.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 from csv import reader
+from os.path import expanduser
 from sys import argv
 if __name__ == "__main__":
     if len(argv) != 3:
         print("Usage: %s <input_csv> <output_csv>" % argv[0]); exit(1)
-    mask = dict(); out = open(argv[2], 'w')
-    for u,v,s in reader(open(argv[1])):
+    mask = dict(); out = open(expanduser(argv[2]), 'w')
+    for u,v,s in reader(open(expanduser(argv[1]))):
         if s.strip() == 'Similarity':
             continue
         u = u.strip(); v = v.strip()
