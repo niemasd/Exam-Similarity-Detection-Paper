f71dd0282d3e6b79542295dcec0a8828f7de1e5c
Niema Moshiri <niemamoshiri@gmail.com>
2021-07-23 10:17:37 -0700

    Added script to anonymize similarity scores

---
+++ scripts/anonymize_similarity_csv.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+from csv import reader
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Usage: %s <input_csv> <output_csv>" % argv[0]); exit(1)
+    mask = dict(); out = open(argv[2], 'w')
+    for u,v,s in reader(open(argv[1])):
+        if s.strip() == 'Similarity':
+            continue
+        u = u.strip(); v = v.strip()
+        if u not in mask:
+            mask[u] = len(mask)
+        if v not in mask:
+            mask[v] = len(mask)
+        out.write('%d,%d,%s\n' % (mask[u],mask[v],s))
+    out.close()
